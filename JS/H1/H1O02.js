337b2db952becaa2e41bb2a4113c098fa099c022
JustLuuk <151666@csgaugustinus.nl>
2019-11-18 09:08:11 +0000

    vormen

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,12 +1,16 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   noStroke();
-  fill('darkred');
-  triangle(225,0,0,450,450,450);
+  fill('pink');
+  triangle(450,0,0,225,450,450);
+
+   noStroke();
+  fill('purple');
+  triangle(450,225,0,0,0,450);
 }
