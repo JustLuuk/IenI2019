a5d005a19322e36428a2a61a1d7715d72cd60472
JustLuuk <151666@csgaugustinus.nl>
2019-11-26 09:29:34 +0000

    een paar opdrachten

--- JS/H1/H1O13.js
+++ JS/H1/H1O13.js
@@ -14,14 +14,17 @@ function draw() {
   text("xJOS: " + round(xJOS) + " (mouseX:" + round(mouseX) + ")",10,20);
   text("yJOS: " + round(yJOS) + " (mouseY:" + round(mouseY) + ")",260,20);
   
-  xJOS = constrain(mouseX,100,450);
+  xJOS = constrain(mouseX, 75,375);
+yJOS = constrain(mouseY, 75,375);
   
   scale(1);
-  translate(xJOS,225);
+  translate(xJOS, yJOS, 225);
 
   // in de volgende regels wordt JOS getekend
 
+   
   push();
+  scale(0.5);
   noStroke();
   fill('indianred');
   ellipse(0,0,150);
@@ -38,6 +41,7 @@ function draw() {
   fill('white');
   arc(0, 40, 80, 40, 0, PI, CHORD);
   pop();
+ 
   // einde tekenen JOS
   
 }
