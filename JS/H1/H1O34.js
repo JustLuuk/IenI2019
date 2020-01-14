10f017d8d8a2121098bf6422549739a4b16f9693
JustLuuk <151666@csgaugustinus.nl>
2020-01-14 13:16:51 +0000

    veel

--- JS/H1/H1O34.js
+++ JS/H1/H1O34.js
@@ -1,18 +1,31 @@
-var aantal = 100;
+var aantal = 20;
 var x;
 var y;
 var diameter;
+var R;
+var G;
+var B;
 
 function setup() {
   canvas = createCanvas(451,451);
   canvas.parent('processing');
   frameRate(5);
-  noLoop();
+  //noLoop();
   strokeWeight(4);
-  stroke('steelblue');
+  stroke(R,G,B);
 }
 
 function draw() {
   background('white');
+  for (var n = 0; n < aantal; n ++){
+  x = random(50,400);
+  y = random(50,400);
+  diameter = random(25,75);
+  ellipse(x,y,diameter);
+  R = random(1,256);
+  G = random(1,256);
+  B = random(1,256);
+  
+  }
 
 }
